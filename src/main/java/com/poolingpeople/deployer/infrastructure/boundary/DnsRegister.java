0d70f12266c02ba585757a137dfa89a090867fff
alacambra <alacambra@gmail.com>
2015-03-23 18:20:55 +0100

    bug fixed

---
+++ src/main/java/com/poolingpeople/deployer/infrastructure/boundary/DnsRegister.java
@@ -0,0 +1,14 @@
+package com.poolingpeople.deployer.infrastructure.boundary;
+
+/**
+ * Created by alacambra on 23.03.15.
+ */
+public class DnsRegister {
+
+    // get current instances
+    // locate those that needs domain on R53
+    // update existent records
+    // create new records
+    // remove old records
+
+}
