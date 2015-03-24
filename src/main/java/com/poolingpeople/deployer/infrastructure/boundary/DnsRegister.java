df0b15d176989c3c3610c5f9fc9e349d8f557ead
alacambra <alacambra@gmail.com>
2015-03-24 17:42:43 +0100

    getting ec2 descriptions from server

--- src/main/java/com/poolingpeople/deployer/infrastructure/boundary/DnsRegister.java
+++ src/main/java/com/poolingpeople/deployer/infrastructure/boundary/DnsRegister.java
@@ -1,9 +1,96 @@
 package com.poolingpeople.deployer.infrastructure.boundary;
 
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.DescribeInstancesResult;
+import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.Reservation;
+import com.amazonaws.services.ec2.model.Tag;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Created by alacambra on 23.03.15.
  */
 public class DnsRegister {
+
+    String accessKey;
+    String secretKey;
+
+    public List<Instance> getInstances(){
+        loadAWSCredentials();
+        AmazonEC2 ec2 = new AmazonEC2Client(new BasicAWSCredentials(accessKey, secretKey));
+        ec2.setRegion(Region.getRegion(Regions.EU_WEST_1));
+        DescribeInstancesResult result = ec2.describeInstances();
+
+        List<Instance> instances = result.getReservations().stream().map(res -> (Instance) res.getInstances().get(0)).collect(Collectors.toList());
+
+        instances.stream().map(
+                instance -> instance.getTags().stream()
+                        .filter(tag -> !tag.getKey().equals("name"))
+                        .map(tag -> tag.getValue()).findFirst().get()).forEach(System.out::println);
+//                        .findFirst())
+
+//                .collect(Collectors.toList()).stream().forEach(System.out::println);
+
+        return null;
+    }
+
+    private void something(Collection<String> s){
+
+        System.out.println(s);
+
+    }
+
+
+    /*
+    @todo: extract to utils lib
+     */
+    private void loadAWSCredentials(){
+
+        accessKey = System.getenv("aws-access-key");
+        secretKey = System.getenv("aws-secret-key");
+
+        if( accessKey != null && accessKey != null ) return;
+
+        InputStream akStream = getClass().getClassLoader().getResourceAsStream("aws-access-key");
+        InputStream skStream = getClass().getClassLoader().getResourceAsStream("aws-secret-key");
+
+        if( akStream == null || skStream == null ){
+            throw new RuntimeException("aws keys not found");
+        }
+
+        accessKey = streamToString(akStream);
+        secretKey = streamToString(skStream);
+
+    }
+
+    private String streamToString(InputStream in)  {
+
+        try {
+
+            StringBuilder out = new StringBuilder();
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+            for (String line = br.readLine(); line != null; line = br.readLine()) {
+                out.append(line);
+            }
+
+            br.close();
+            return out.toString();
+
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
 
     // get current instances
     // locate those that needs domain on R53
---
+++ src/test/java/com/poolingpeople/deployer/infrastructure/boundary/DnsRegisterTest.java
@@ -0,0 +1,15 @@
+package com.poolingpeople.deployer.infrastructure.boundary;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DnsRegisterTest {
+
+    DnsRegister cut = new DnsRegister();
+
+    @Test
+    public void testGetInstances() throws Exception {
+        cut.getInstances();
+    }
+}
